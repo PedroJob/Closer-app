10093a2cd5594a40fc273d10e0d882d71c64857f
Pedro Job <pedrojv2906@gmail.com>
2024-04-17 21:44:37 -0300

    add web application folder

--- src/main/java/closer/models/user.java
+++ src/main/java/closer/models/user.java
@@ -7,11 +7,53 @@ public class user {
     private int UserID;
     private String UserProfilePic;
 
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getUserEmail() {
+        return UserEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        UserEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int userID) {
+        UserID = userID;
+    }
+
+    public String getUserProfilePic() {
+        return UserProfilePic;
+    }
+
+    public void setUserProfilePic(String userProfilePic) {
+        UserProfilePic = userProfilePic;
+    }
+
     public user(String userName, String userEmail, String userPassword, int userID, String userProfilePic) {
         UserName = userName;
         UserEmail = userEmail;
         UserPassword = userPassword;
         UserID = userID;
         UserProfilePic = userProfilePic;
+
+
     }
 }
